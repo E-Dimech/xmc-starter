0b230a8f3921a40ef8152ad4ce495b2e5dc74a50
E-Dimech <edimech@fishtank.ca>
2024-06-05 11:25:44 -0400

    Add mock data and decorators to Storybook preview

---
+++ src/sxastarter/.storybook/preview.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LayoutServicePageState, SitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
+import { componentBuilder } from '../src/temp/componentBuilder';
+import type { Preview } from '@storybook/react';
+
+import 'src/assets/main.scss';
+
+export const mockLayoutData = {
+  sitecore: {
+    context: {
+      pageEditing: false,
+      pageState: LayoutServicePageState.Normal,
+    },
+    setContext: () => {},
+    route: null,
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <SitecoreContext
+        componentFactory={componentBuilder.getComponentFactory({ isEditing: mockLayoutData.sitecore.context.pageEditing })}
+        layoutData={mockLayoutData}
+      >
+        <Story />
+      </SitecoreContext>
+    ),
+  ],
+};
+
+export default preview;
