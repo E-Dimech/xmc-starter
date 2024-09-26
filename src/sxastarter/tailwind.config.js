7ad5c509357bc04e4cf4958eb6e67b2de4b9b587
E-Dimech <edimech@fishtank.ca>
2024-09-26 16:09:23 -0400

    update tailwind config

--- src/sxastarter/tailwind.config.js
+++ src/sxastarter/tailwind.config.js
@@ -13,27 +13,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "brand-green": "#4DF69B",
-        "brand-amber": "#FF8656",
-        "brand-red": "#FF5656",
-        "brand-gray": "#7E7E7E",
+        'brand-green': '#4DF69B',
+        'brand-amber': '#FF8656',
+        'brand-red': '#FF5656',
+        'brand-gray': '#7E7E7E',
       },
       fontSize: {
-        'xs': '.75rem',      // 12px
-        'sm': '.875rem',     // 14px
-        'base': '1rem',      // 16px
-        'lg': '1.125rem',    // 18px
-        'xl': '1.25rem',     // 20px
-        '2xl': '1.5rem',     // 24px
-        '3xl': '1.875rem',   // 30px
-        '4xl': '2.25rem',    // 36px
-        '5xl': '3rem',       // 48px
-        '6xl': '3.75rem',    // 60px
-        '7xl': '4.5rem',     // 72px
-        '8xl': '6rem',       // 96px
-        '9xl': '8rem',       // 128px
+        xs: '.75rem', // 12px
+        sm: '.875rem', // 14px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem', // 72px
+        '8xl': '6rem', // 96px
+        '9xl': '8rem', // 128px
+      },
+      boxShadow: {
+        'custom-inset': 'inset 0 0 0 16px rgba(255, 255, 255, 0.1)',
+      },
     },
   },
-},
   plugins: [],
-}
+};
