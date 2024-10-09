2fdb39601bf71136c03dc4dabcf0a48dc356f476
E-Dimech <edimech@fishtank.ca>
2024-10-09 10:52:51 -0400

    Carousel component INIT

---
+++ src/sxastarter/src/stories/CarouselComponent.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CarouselComponent from 'src/components/CarouselComponent';
+import force from '../assets/images/force.jpg';
+import helix from '../assets/images/helix.jpg';
+import alpenFury from '../assets/images/alpenFury.jpg';
+
+const meta = {
+  title: 'Components/CarouselComponent',
+  component: CarouselComponent,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof CarouselComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const mockImageField = (src: string, alt: string) => ({
+  value: {
+    src,
+    alt,
+  },
+});
+
+export const Default: Story = {
+  args: {
+    fields: {
+      slides: [
+        {
+          slideImage: mockImageField(force.src, 'Force'),
+          slideAltText: {
+            value: 'Force',
+          },
+          slideCaption: {
+            value: 'Millennium Force',
+          },
+        },
+        {
+          slideImage: mockImageField(helix.src, 'Helix'),
+          slideAltText: {
+            value: 'Helix',
+          },
+          slideCaption: {
+            value: 'Helix',
+          },
+        },
+        {
+          slideImage: mockImageField(alpenFury.src, 'Alpen Fury'),
+          slideAltText: {
+            value: 'Alpen Fury',
+          },
+          slideCaption: {
+            value: 'Alpen Fury',
+          },
+        },
+      ],
+    },
+  },
+};
